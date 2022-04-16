38304ab1c3f5886df4d8253debd114028780948e
Dmitriy <dima.komlyk@gmail.com>
2022-04-16 12:14:57 +0300

    Add open modal function

--- src/app/components/chat/Channels.jsx
+++ src/app/components/chat/Channels.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Col, Button, Nav } from 'react-bootstrap';
 import { BsPlus } from 'react-icons/bs';
 import { selectors } from '../../slices/channelsSlice.js';
+import { showModal } from '../../slices/modalsSlice.js';
 
 import ChannelItem from './ChannelItem.jsx';
 
 function Channels() {
+  const dispatch = useDispatch();
   const channels = useSelector(selectors.selectAll);
+
+  const openModal = () => dispatch(showModal({ type: 'addChannel' }));
 
   return (
     <Col xs={4} md={2} className="border-end pt-5 px-0 bg-light">
       <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
         <span>Каналы</span>
-        <Button type="button" size="sm" variant="outline-primary" className="p-0">
+        <Button
+          type="button"
+          size="sm"
+          variant="outline-primary"
+          className="p-0"
+          onClick={openModal}
+        >
           <BsPlus size={20} />
         </Button>
       </div>
