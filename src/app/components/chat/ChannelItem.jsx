c37bdb6aafbabf024a9aca71ca16c66b08c0641d
Dmitriy <dima.komlyk@gmail.com>
2022-04-16 12:12:18 +0300

    Create function for toggle channels. Add Dropdown button with remove, rename functions

--- src/app/components/chat/ChannelItem.jsx
+++ src/app/components/chat/ChannelItem.jsx
@@ -1,20 +1,65 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Nav, Button } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Nav,
+  Button,
+  ButtonGroup,
+  Dropdown,
+} from 'react-bootstrap';
+
+import { showModal } from '../../slices/modalsSlice.js';
+import { setCurrentChannel } from '../../slices/channelsSlice.js';
 
 function ChannelItem({ channel }) {
+  const dispatch = useDispatch();
   const id = useSelector((state) => state.channels.selectedChannel);
+
+  const handleRemoveChannel = () => {
+    dispatch(showModal({ type: 'removeChannel', item: channel }));
+  };
+
+  const handleRenameChannel = () => {
+    dispatch(showModal({ type: 'renameChannel', item: channel }));
+  };
+
+  const handleChangeChannel = () => {
+    dispatch(setCurrentChannel(channel.id));
+  };
+
+  const renderButton = () => (
+    <Button
+      type="button"
+      variant={channel.id === id ? 'secondary' : 'light'}
+      className="w-100 text-start text-truncate"
+      onClick={handleChangeChannel}
+    >
+      <span className="me-1">#</span>
+      { channel.name }
+    </Button>
+  );
 
   return (
     <Nav.Item as="li" className="w-100">
-      <Button
-        type="button"
-        variant={channel.id === id ? 'secondary' : 'light'}
-        className="rounded-0 w-100 text-start"
-      >
-        <span className="me-1">#</span>
-        { channel.name }
-      </Button>
+      { channel.removable
+        ? (
+          <Dropdown className="w-100" as={ButtonGroup}>
+            { renderButton() }
+            <Dropdown.Toggle split variant={channel.id === id ? 'secondary' : 'light'} id="dropdown-split-basic" />
+            <Dropdown.Menu>
+              <Dropdown.Item
+                onClick={handleRemoveChannel}
+              >
+                Удалить
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={handleRenameChannel}
+              >
+                Переименовать
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        )
+        : renderButton()}
     </Nav.Item>
   );
 }
