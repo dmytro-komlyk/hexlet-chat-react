142451571e750fed5478ac8f84ba021cbf809ccb
Dmitriy <dima.komlyk@gmail.com>
2022-04-16 12:00:27 +0300

    Add SocketProvider, AuthProvider

--- src/app/init.jsx
+++ src/app/init.jsx
@@ -1,16 +1,21 @@
 // @ts-check
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './App.jsx';
 import store from './slices/index.js';
 
-export default () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('chat'),
-  );
-};
+import { AuthProvider } from './contexts/authContext.jsx';
+import { SocketProvider } from './contexts/socketContext.jsx';
+
+const init = async () => (
+  <Provider store={store}>
+    <SocketProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </SocketProvider>
+  </Provider>
+);
+
+export default init;
