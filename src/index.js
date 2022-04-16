cb09fbe1dbb1111c230b73eee5a7eef936545175
Dmitriy <dima.komlyk@gmail.com>
2022-04-16 11:59:31 +0300

    Create async function App

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
-
+import ReactDOM from 'react-dom';
 import init from './app/init.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-init();
+const app = async () => {
+  const vDom = await init();
+  ReactDOM.render(vDom, document.getElementById('chat'));
+};
+
+app();
