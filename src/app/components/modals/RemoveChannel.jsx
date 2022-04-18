fd7abdc2988f42f86e774584e4b14383e5c7f55e
Dmitriy <dima.komlyk@gmail.com>
2022-04-18 20:12:53 +0300

    Deleting a comment

--- src/app/components/modals/RemoveChannel.jsx
+++ src/app/components/modals/RemoveChannel.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Modal, Button } from 'react-bootstrap';
 import useSocket from '../../hooks/useSocket.jsx';
-import { selectors, setCurrentChannel } from '../../slices/channelsSlice.js';
 import { hideModal } from '../../slices/modalsSlice.js';
 
 function RemoveChannel() {
   const dispatch = useDispatch();
   const { removeChannel } = useSocket();
   const { show, item } = useSelector((state) => state.modals);
-  const generalChannel = useSelector(selectors.selectAll).find(({ name }) => name === 'general');
-
-  const moveToChannel = (id) => dispatch(setCurrentChannel(id));
 
   const closeModal = () => dispatch(hideModal());
 
   const generateOnRemove = async () => {
     removeChannel(item, ({ status }) => {
       if (status === 'ok') {
-        moveToChannel(generalChannel.id);
         closeModal();
       }
     });
