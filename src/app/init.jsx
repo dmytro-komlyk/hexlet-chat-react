f89aca4f692942d855998c46d5d6ac1600e3f16d
Dmitriy <dima.komlyk@gmail.com>
2022-04-19 19:51:46 +0300

    Add I18nextProvider

--- src/app/init.jsx
+++ src/app/init.jsx
@@ -1,20 +1,25 @@
 // @ts-check
 import React from 'react';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 
 import App from './App.jsx';
+
 import store from './slices/index.js';
+import i18n from './contexts/i18nContext.jsx';
 
 import { AuthProvider } from './contexts/authContext.jsx';
 import { SocketProvider } from './contexts/socketContext.jsx';
 
 const init = async () => (
   <Provider store={store}>
-    <SocketProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </SocketProvider>
+    <I18nextProvider i18n={i18n}>
+      <SocketProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </SocketProvider>
+    </I18nextProvider>
   </Provider>
 );
 
