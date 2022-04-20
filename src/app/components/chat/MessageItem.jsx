130e38cbf919077f8b1a7e5a9028d048605223c2
Dmitriy <dima.komlyk@gmail.com>
2022-04-20 15:20:04 +0300

    Add filter for message

--- src/app/components/chat/MessageItem.jsx
+++ src/app/components/chat/MessageItem.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { filter } from '../../contexts/i18nContext.jsx';
 
 function MessageItem({ message }) {
+  const { username, value } = message;
+  const cleanedValue = filter.check(value) ? filter.clean(value) : value;
+
   return (
     <div className="text-break mb-2">
-      <b>{message.username}</b>
+      <b>{username}</b>
       :
       {' '}
-      {message.value}
+      {cleanedValue}
     </div>
   );
 }
