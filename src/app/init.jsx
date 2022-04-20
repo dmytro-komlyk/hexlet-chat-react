ec16081c70c3b1ed24d7b0e6a532b339d4af5f78
Dmitriy <dima.komlyk@gmail.com>
2022-04-20 20:32:43 +0300

    Update file

--- src/app/init.jsx
+++ src/app/init.jsx
@@ -1,14 +1,13 @@
 // @ts-check
 import React from 'react';
 import { Provider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
 import { Provider as RollbarProvider } from '@rollbar/react';
 
 import App from './App.jsx';
 
 import store from './slices/index.js';
-import i18n from './contexts/i18nContext.jsx';
 
+import { I18nProvider } from './contexts/i18nContext.jsx';
 import { AuthProvider } from './contexts/authContext.jsx';
 import { SocketProvider } from './contexts/socketContext.jsx';
 
@@ -24,13 +23,13 @@ const rollbarConfig = {
 const init = async () => (
   <RollbarProvider config={rollbarConfig}>
     <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <SocketProvider>
+      <SocketProvider>
+        <I18nProvider>
           <AuthProvider>
             <App />
           </AuthProvider>
-        </SocketProvider>
-      </I18nextProvider>
+        </I18nProvider>
+      </SocketProvider>
     </Provider>
   </RollbarProvider>
 );
