239acbfb581b9101e6a99d0a104f0254509f0167
Dmitriy <dima.komlyk@gmail.com>
2022-04-20 16:51:39 +0300

    Add RollbarProvider

--- src/app/init.jsx
+++ src/app/init.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
+import { Provider as RollbarProvider } from '@rollbar/react';
 
 import App from './App.jsx';
 
@@ -11,16 +12,27 @@ import i18n from './contexts/i18nContext.jsx';
 import { AuthProvider } from './contexts/authContext.jsx';
 import { SocketProvider } from './contexts/socketContext.jsx';
 
+const rollbarConfig = {
+  accessToken: 'cfb004f2884d42d7aae180b7d690074b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
+};
+
 const init = async () => (
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <SocketProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </SocketProvider>
-    </I18nextProvider>
-  </Provider>
+  <RollbarProvider config={rollbarConfig}>
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <SocketProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </SocketProvider>
+      </I18nextProvider>
+    </Provider>
+  </RollbarProvider>
 );
 
 export default init;
