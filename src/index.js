727222abbc381712e8a051482d153ce9e8e2d353
Dmitriy <dima.komlyk@gmail.com>
2022-04-21 16:06:17 +0300

    Add socket init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
 
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
 import init from './app/init.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
@@ -11,7 +12,8 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = async () => {
-  const vDom = await init();
+  const socket = io();
+  const vDom = await init(socket);
   ReactDOM.render(vDom, document.getElementById('chat'));
 };
 
